a3b83a83b4b03a18850d4eaa4027210aa258350a
Nicolas Oris <nicolasoris98@gmail.com>
2023-08-03 21:50:37 -0300

    agregado dotenv

--- src/db.ts
+++ src/db.ts
@@ -4,14 +4,15 @@ import { Role } from "./entities/Role"
 import { SnakeNamingStrategy } from "typeorm-naming-strategies"
 import { Schedule } from "./entities/Schedule"
 import { Shift } from "./entities/Shift"
+require('dotenv').config({ path: './.env' });
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "postgres",
-    database: "shift_db",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     synchronize: true,
     logging: true,
     entities: [User, Role, Schedule, Shift],
